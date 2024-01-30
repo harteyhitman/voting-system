2420d032fb5eaa4685506dad23c4074cfff16f75
harteyhitman <oyegoke.a18@gmail.com>
2024-01-30 17:03:54 +0100

    toggle-min

--- src/components/ToggleMin.jsx
+++ src/components/ToggleMin.jsx
@@ -1,27 +1,44 @@
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import React, { useState } from 'react'
+
+
+
 const ToggleMin = () => {
   const [activeLink, setActiveLink] = useState(null);
 
+
   const handleLinkClick = (link) => {
     setActiveLink(link);
-  };
-  const navigate = useNavigate();
+
+    const elements = document.querySelectorAll('.toggle-min div');
+    elements.forEach((element) => {
+      const isClickedElement = element.getAttribute('data-link') === link;
+      element.style.background = isClickedElement ? '#0CBB5C' : '';
+      element.style.color = isClickedElement ? '#FFFFFF' : '';
+    });
+  }
 
 
   return (
     <div className="toggle-min" >
-      <div onClick={() => handleLinkClick("/Login/Admin")}
-        style={{
-          background: activeLink === "/Login/Admin" ? "#0CBB5C" : "",
-          color: activeLink === "/Login/Admin" ? "#FFFFFF" : "",}} className="user">User </div>
-
-
-      <div onClick={() => handleLinkClick("/Login/Admin/UserAccount")}
-        style={{
-          background: activeLink === "/Login/Admin/UserAccount" ? "#0CBB5C" : "",
-          color: activeLink === "/Login/Admin" ? "#FFFFFF" : "",}} className="admin">Admin</div>
-
+      
+ <div
+        onClick={() => 
+          handleLinkClick("/Login/Admin")
+       }
+        data-link="/Login/Admin"
+        className="user"
+      >
+        User
+      </div>
+      <div
+        onClick={() => 
+          handleLinkClick("/Login/Admin/UserAccount")
+       }
+        data-link="/Login/Admin/UserAccount"
+        className="admin"
+      >
+        Admin
+      </div>
     </div>
   );
 };
